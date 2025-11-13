a775c4d31b771c173375a1664ac19d274c7901c6
QingFeng-awa <qingfeng_awa@163.com>
2025-11-13 11:18:20 +0800

    fix

--- main.py
+++ main.py
@@ -8,5 +8,6 @@ class TestAstrbot(Star):
     @filter.command("get_group_id")
     async def get_group_id(self, event: AstrMessageEvent):
         """获取当前群 ID，对应 Astrbot 方法`event.get_group_id()`。"""
-        yield event.plain_result(event.get_group_id())
-        yield event.plain_result(type(event.get_group_id()))
+        group_id = event.get_group_id()
+        response_type = type(event.get_group_id())
+        yield event.plain_result(str(group_id), "type = " + str(response_type))
