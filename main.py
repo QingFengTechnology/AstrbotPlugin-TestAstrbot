9361827626ad351ef9717dc85587681a503b7424
QingFeng-awa <qingfeng_awa@163.com>
2025-12-07 17:41:14 +0800

    Test return。

--- main.py
+++ main.py
@@ -52,3 +52,6 @@ class TestAstrbot(Star):
             logger.debug("logger.debug")
         except Exception as e:
             logger.error(f"Error: {e}")
+    async def test_return_result(self, event: AstrMessageEvent):
+        """测试 AstrBot 是否支持 `return event.plain_result("")`"""
+        return event.plain_result("return event.plain_result")
